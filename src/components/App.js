a9b0364b5b3f49c215206fb6ab8bfb524f131654
sip615 <sunilpatel.328@gmail.com>
2021-01-31 17:52:43 -0500

    refactored the components for one-directional data flow

---
+++ src/components/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Player from './Player';
+
+
+class App extends Component {
+  state = {
+    players: [
+      {
+        name: "Guil",
+        id: 1,
+        score: 0
+      },
+      {
+        name: "Treasure",
+        id: 2,
+        score: 0
+      },
+      {
+        name: "Ashley",
+        id: 3,
+        score: 0
+      },
+      {
+        name: "James",
+        id: 4,
+        score: 0
+      }
+    ]
+  };
+
+  handleRemovePlayer = (id) => {
+    this.setState(prevState => {
+      return {
+        players: prevState.players.filter(p => p.id !== id)
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div className="scoreboard">
+        <Header
+          title="Scoreboard"
+          totalPlayers={this.state.players.length}
+        />
+
+        {/* Players list */}
+        {this.state.players.map(player =>
+          <Player
+            name={player.name}
+            id={player.id}
+            key={player.id.toString()}
+            removePlayer={this.handleRemovePlayer}
+            score={player.score}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
